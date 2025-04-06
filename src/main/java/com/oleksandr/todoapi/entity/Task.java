3d0b8c2080e1d04019962971c357e71a72dcabb6
= <kulbitsanya0@gmail.com>
2025-04-06 15:49:14 +0200

    Created Entity Task

---
+++ src/main/java/com/oleksandr/todoapi/entity/Task.java
@@ -0,0 +1,29 @@
+package com.oleksandr.todoapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Tasks")
+public class Task implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "taskDate")
+    private LocalDate taskDate;
+
+}
