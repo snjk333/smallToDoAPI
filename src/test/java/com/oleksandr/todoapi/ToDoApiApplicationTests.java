35810cc190bd8be8e61cf27ae60f239df28a0cf5
= <kulbitsanya0@gmail.com>
2025-04-06 15:40:57 +0200

    Initial Commit

---
+++ src/test/java/com/oleksandr/todoapi/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleksandr.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
