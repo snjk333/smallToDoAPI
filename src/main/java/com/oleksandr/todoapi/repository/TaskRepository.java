2c62bdf0fc6435e9545a3189fde90db81e35ac15
= <kulbitsanya0@gmail.com>
2025-04-06 17:43:29 +0200

    Add findByTitle and findByTaskDate methods to repository

--- src/main/java/com/oleksandr/todoapi/repository/TaskRepository.java
+++ src/main/java/com/oleksandr/todoapi/repository/TaskRepository.java
@@ -3,5 +3,10 @@ package com.oleksandr.todoapi.repository;
 import com.oleksandr.todoapi.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    Task findByTitle(String title);
+    List<Task> findByTaskDate(LocalDate taskDate);
 }
