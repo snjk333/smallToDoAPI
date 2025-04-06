a699eed86fc94507392729b0d120346d6d3116af
= <kulbitsanya0@gmail.com>
2025-04-06 17:50:10 +0200

    Made simple controller

---
+++ src/main/java/com/oleksandr/todoapi/contoller/TaskController.java
@@ -0,0 +1,38 @@
+package com.oleksandr.todoapi.contoller;
+
+import com.oleksandr.todoapi.dto.TaskDTO;
+import com.oleksandr.todoapi.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todo")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public TaskDTO getTaskByTitle(String title) {
+        return taskService.findByTitle(title);
+    }
+
+    @GetMapping("/date")
+    public List<TaskDTO> getTaskByDate(String date) {
+        return taskService.findTasksByDate(date);
+    }
+
+    @GetMapping("/today")
+    public List<TaskDTO> getTaskByToday() {
+        return taskService.findTodayTasks();
+    }
+
+    @PostMapping
+    public void saveTask(@RequestBody TaskDTO taskDTO) {
+        taskService.saveTask(taskDTO);
+    }
+
+}
