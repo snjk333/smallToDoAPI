312772c8991e62ca9830e58843f6bc17435bc12d
= <kulbitsanya0@gmail.com>
2025-04-06 17:37:10 +0200

    Add test mapFromDTOToEntity for taskMapper.mapToEntity method

--- src/test/java/com/oleksandr/todoapi/mapper/TaskMapperTest.java
+++ src/test/java/com/oleksandr/todoapi/mapper/TaskMapperTest.java
@@ -33,4 +33,24 @@ public class TaskMapperTest {
 
         assertThat(actual).isEqualTo(expectedResult);
     }
+
+    @Test
+    void mapFromDTOToEntity() {
+        TaskDTO dto = TaskDTO.builder()
+                .title("testTitle")
+                .description("testDescription")
+                .taskDate("06-04-2025")
+                .build();
+
+        Task actualResult = taskMapper.mapToEntity(dto);
+
+
+        Task expected = Task.builder()
+                .title("testTitle")
+                .description("testDescription")
+                .taskDate(LocalDate.of(2025,4,6))
+                .build();
+
+        assertThat(actualResult).isEqualTo(expected);
+    }
 }
