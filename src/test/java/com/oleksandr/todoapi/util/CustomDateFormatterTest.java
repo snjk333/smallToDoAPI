689935028bcf456de034215f76b229714512775e
= <kulbitsanya0@gmail.com>
2025-04-06 16:44:33 +0200

    Wrote Tests for CustomDateFormatter before write class. Tests are red...

---
+++ src/test/java/com/oleksandr/todoapi/util/CustomDateFormatterTest.java
@@ -0,0 +1,60 @@
+package com.oleksandr.todoapi.util;
+
+import lombok.experimental.UtilityClass;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CustomDateFormatterTest {
+
+    @Test
+    void formatToStringTest(){
+        LocalDate date = LocalDate.of(2000,12,1);
+        String expectedResult = "01-12-2000";
+
+        String actualResult = CustomDateFormatter.formatToString(date);
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void StringDateHasValidFormatTest(){
+        String dateAsString = "01-12-2000";
+
+        boolean actualResult = CustomDateFormatter.isValid(dateAsString);
+
+        assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    void StringDateHasInvalidFormatTest(){
+        String dateAsString = "01-12-2000 15:12";
+
+        boolean actualResult = CustomDateFormatter.isValid(dateAsString);
+
+        assertThat(actualResult).isFalse();
+    }
+
+
+    @Test
+    void formatToDateSuccessTest(){
+        String dateAsString = "01-12-2000";
+        LocalDate expectedResult = LocalDate.of(2000,12,1);
+
+        LocalDate actualResult = CustomDateFormatter.formatToDate(dateAsString);
+
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    void shouldThrowExceptionIfDataInvalid(){
+        String dateAsString = "01-12-2000 15:12";
+
+        assertThrows(DateTimeParseException.class, () -> CustomDateFormatter.formatToDate(dateAsString));
+    }
+}
