12275976d896c17402a5cc3368e59824ff3c30cc
= <kulbitsanya0@gmail.com>
2025-04-06 16:52:03 +0200

    Made CustomDateFormatter class. Tests are green. All good

--- src/main/java/com/oleksandr/todoapi/mapper/TaskMapper.java
+++ src/main/java/com/oleksandr/todoapi/mapper/TaskMapper.java
@@ -2,15 +2,14 @@ package com.oleksandr.todoapi.mapper;
 
 import com.oleksandr.todoapi.dto.TaskDTO;
 import com.oleksandr.todoapi.entity.Task;
-import com.oleksandr.todoapi.formatter.CustomDateFormatter;
+import com.oleksandr.todoapi.util.CustomDateFormatter;
 
 public class TaskMapper {
-    CustomDateFormatter formatter = new CustomDateFormatter();
     public TaskDTO mapToDTO(Task task) {
         return TaskDTO.builder()
                 .title(task.getTitle())
                 .description(task.getDescription())
-                .taskDate(formatter.format(task.getTaskDate()))
+                .taskDate(CustomDateFormatter.formatToString(task.getTaskDate()))
                 .build();
     }
 }
--- src/main/java/com/oleksandr/todoapi/util/CustomDateFormatter.java
+++ src/main/java/com/oleksandr/todoapi/util/CustomDateFormatter.java
@@ -1,4 +1,37 @@
 package com.oleksandr.todoapi.util;
 
+import lombok.experimental.UtilityClass;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
+
+@UtilityClass
 public class CustomDateFormatter {
+
+    private static final String PATTERN = "dd-MM-yyyy";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(PATTERN);
+
+    public static LocalDate formatToDate(String dateAsString) {
+        return LocalDate.parse(dateAsString, FORMATTER);
+    }
+
+    public static boolean isValid(String dateAsString) {
+        try {
+            LocalDate.parse(dateAsString, FORMATTER);
+            return true;
+        } catch (DateTimeParseException exception) {
+            return false;
+        }
+    }
+
+    public static String formatToString(LocalDate date) {
+        return FORMATTER.format(date);
+    }
 }
